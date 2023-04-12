e4cb1abfa3a1ffc8daa836f8af1190e07ddb3082
ibrahim-mohamed-rezk <ibrahimmo989800@gmail.com>
2023-04-12 11:08:51 +0200

    put

--- app.js
+++ app.js
@@ -48,7 +48,10 @@ app.use(function(req, res, next) {
     res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Methods', '*');
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
   }
 
   next();
